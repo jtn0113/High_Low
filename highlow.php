2f2e3af16a9bc173bcd96083bba9000899eebd7c
John Nichols <jtn0113@yahoo.com>
2017-03-14 14:21:56 -0500

    added user input to high low

--- highlow.php
+++ highlow.php
@@ -1,8 +1,13 @@
 <?php 
 
-$random = mt_rand(1, 100);
+if(!is_numeric($argv[1]) || !is_numeric($argv[2])) {
+	fwrite(STDERR, "Both arguments must be numbers!\n");
+	exit(1);
+};
 
-fwrite(STDOUT, "What is your guess?(1-100)\n");
+$random = mt_rand($argv[1], $argv[2]);	
+
+fwrite(STDOUT, "What is your guess?($argv[1]-$argv[2])\n");
 
 $userGuess = fgets(STDIN);
 
@@ -12,12 +17,12 @@ do {
 	if ($userGuess > $random) {
 		$guesses ++;
 		echo "LOWER\n";
-		fwrite(STDOUT, "What is your guess?(1-100)\n");
+		fwrite(STDOUT, "What is your guess?($argv[1]-$argv[2])\n");
 		$userGuess = fgets(STDIN);
 	} else if ($userGuess < $random) {
 		$guesses ++;
 		echo "HIGHER\n";
-		fwrite(STDOUT, "What is your guess?(1-100)\n");
+		fwrite(STDOUT, "What is your guess?($argv[1]-$argv[2])\n");
 		$userGuess = fgets(STDIN);
 	}
 } while ($userGuess > $random || $userGuess < $random);
@@ -28,4 +33,4 @@ if ($userGuess == $random) {
 	echo "You toook $guesses guesses!\n";
 }
 
- ?>
+?>
